f88b930fba1d9607d9dd343058875c854363672a
MRamovic <56627280+MRamovic@users.noreply.github.com>
2021-02-16 21:16:21 +0100

    Add project files.

---
+++ BlazorApp16/Client/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+
+namespace BlazorApp16.Client
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("app");
+
+			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			await builder.Build().RunAsync();
+		}
+	}
+	public static class FileUtil
+	{
+		public async static Task Save(IJSRuntime js, string fn, byte[] data)
+		{
+			await js.InvokeAsync<object>(
+				"snimiBajte",
+				Convert.ToBase64String(data),
+				fn);
+		}
+	}
+}
